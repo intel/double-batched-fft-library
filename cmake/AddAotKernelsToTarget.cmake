d29c78c81c5583fc7e4ce20d8f352cad31ae63dd
Carsten Uphoff <carsten.uphoff@intel.com>
2023-04-26 00:08:54 -0700

    Support link scopes in add_aot_kernels_to_target
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- cmake/AddAotKernelsToTarget.cmake
+++ cmake/AddAotKernelsToTarget.cmake
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 function(add_aot_kernels_to_target)
-    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "TARGET;PREFIX;DEVICE" "LIST")
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "NO_LINK_SCOPE" "TARGET;PREFIX;DEVICE;LINK_SCOPE" "LIST")
     set(BIN_FILE "${ARG_PREFIX}.bin")
     set(OBJ_FILE "${ARG_PREFIX}.o")
     add_custom_command(
@@ -17,6 +17,18 @@ function(add_aot_kernels_to_target)
     set_property(TARGET fft-aot-${PREFIX}
                  PROPERTY IMPORTED_OBJECTS "${CMAKE_CURRENT_BINARY_DIR}/${OBJ_FILE}")
 
-    target_link_libraries(${ARG_TARGET} PRIVATE fft-aot-${PREFIX})
+    set(SUPPORTED_LINK_SCOPES PRIVATE INTERFACE PUBLIC)
+
+    if(ARG_NO_LINK_SCOPE)
+        target_link_libraries(${ARG_TARGET} fft-aot-${PREFIX})
+    else()
+        if(NOT DEFINED ARG_LINK_SCOPE)
+            set(ARG_LINK_SCOPE PRIVATE)
+        endif()
+        if(NOT ${ARG_LINK_SCOPE} IN_LIST SUPPORTED_LINK_SCOPES)
+            message(SEND_ERROR "Link scope must be PRIVATE, INTERFACE, or PUBLIC. (${ARG_LINK_SCOPE} given)")
+        endif()
+        target_link_libraries(${ARG_TARGET} ${ARG_LINK_SCOPE} fft-aot-${PREFIX})
+    endif()
 endfunction()
 
