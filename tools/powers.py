72e284d4b9c61ccbffaeb3c300e12e46fba31c47
Carsten Uphoff <carsten.uphoff@intel.com>
2023-11-03 08:16:09 -0700

    Make powers.py more flexible
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- tools/powers.py
+++ tools/powers.py
@@ -3,10 +3,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import argparse
+import itertools
 import math
+
+primes = [2, 3, 5, 7, 11, 13]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', action='store_true')
+parser.add_argument('-p', type=int, default=len(primes), choices=range(1,len(primes)+1))
 parser.add_argument('min', type=int, default=2)
 parser.add_argument('max', type=int, default=512)
 args = parser.parse_args()
@@ -15,13 +19,13 @@ def ilog(base, x):
     return int(math.ceil(math.log(x, base)))
 
 powers = []
-for i in range(1 if args.e else 0, ilog(2, args.max) + 1):
-    for j in range(ilog(3, args.max) + 1):
-        for k in range(ilog(5, args.max) + 1):
-            for m in range(ilog(7, args.max) + 1):
-                for n in range(ilog(11, args.max) + 1):
-                    for o in range(ilog(13, args.max) + 1):
-                        p = 2**i * 3**j * 5**k * 7**m * 11**n * 13**o
-                        if args.min <= p and p <= args.max:
-                            powers.append(p)
+exponents = [list(range(ilog(primes[i], args.max + 1))) for i in range(args.p)]
+if args.e:
+    exponents[0] = list(range(1, ilog(2, args.max) + 1))
+for es in itertools.product(*exponents):
+    p = 1
+    for i, e in enumerate(es):
+        p *= pow(primes[i], e)
+    if args.min <= p and p <= args.max:
+        powers.append(p)
 print(' '.join([str(i) for i in sorted(powers)]))
