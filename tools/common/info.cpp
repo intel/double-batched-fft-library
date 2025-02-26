3931f3adae5325aa0b51084d9dd18a69e73477c7
Carsten Uphoff <carsten.uphoff@intel.com>
2025-02-26 14:02:24 +0100

    Add dg2 device info
    Signed-off-by: Carsten Uphoff <carsten.uphoff@intel.com>

--- tools/common/info.cpp
+++ tools/common/info.cpp
@@ -6,4 +6,5 @@
 using namespace bbfft;
 
 const std::unordered_map<std::string, device_info> builtin_device_info = {
-    {"pvc", {1024, {16, 32}, 128 * 1024, device_type::gpu}}};
+    {"pvc", {1024, {16, 32}, 128 * 1024, device_type::gpu}},
+    {"dg2", {512, {8, 16, 32}, 64 * 1024, device_type::gpu}}};
